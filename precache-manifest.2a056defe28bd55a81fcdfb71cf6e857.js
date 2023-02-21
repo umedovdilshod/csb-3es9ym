27f9c4122a7d9b08a151ef5da0ad50431e1f5956
Code Sandbox <hello@codesandbox.io>
2023-02-21 04:27:41 +0000

    Deployment from Codesandbox.

---
+++ precache-manifest.2a056defe28bd55a81fcdfb71cf6e857.js
@@ -0,0 +1,206 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bec87c618ba31444790757b75812cc77",
+    "url": "https://umedovdilshod.github.io/csb-3es9ym/index.html"
+  },
+  {
+    "revision": "00dab37f36090ffb9d60",
+    "url": "https://umedovdilshod.github.io/csb-3es9ym/static/css/main.176b7693.chunk.css"
+  },
+  {
+    "revision": "265f312d69f585c42499",
+    "url": "https://umedovdilshod.github.io/csb-3es9ym/static/js/2.6c8c48b2.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "https://umedovdilshod.github.io/csb-3es9ym/static/js/2.6c8c48b2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "00dab37f36090ffb9d60",
+    "url": "https://umedovdilshod.github.io/csb-3es9ym/static/js/main.b3f1a3f1.chunk.js"
+  },
+  {
+    "revision": "6f7ca747c9d374754342",
+    "url": "https://umedovdilshod.github.io/csb-3es9ym/static/js/runtime-main.309aad54.js"
+  },
+  {
+    "revision": "183de736efe789394f26ed013b0b5cdd",
+    "url": "https://umedovdilshod.github.io/csb-3es9ym/static/media/Calibre-Light.183de736.woff2"
+  },
+  {
+    "revision": "62b8f0fd78f4d842b3a75c1d2512e449",
+    "url": "https://umedovdilshod.github.io/csb-3es9ym/static/media/Calibre-Light.62b8f0fd.woff"
+  },
+  {
+    "revision": "73b7f4385ae2f8028e1b365a48038582",
+    "url": "https://umedovdilshod.github.io/csb-3es9ym/static/media/Calibre-Light.73b7f438.ttf"
+  },
+  {
+    "revision": "1222cf0d45062719e06c3d7d44937e8f",
+    "url": "https://umedovdilshod.github.io/csb-3es9ym/static/media/Calibre-LightItalic.1222cf0d.woff"
+  },
+  {
+    "revision": "3ede1be73648125ed40cfd07e1292bf1",
+    "url": "https://umedovdilshod.github.io/csb-3es9ym/static/media/Calibre-LightItalic.3ede1be7.ttf"
+  },
+  {
+    "revision": "f34c1430353885b55d271d473235eb6a",
+    "url": "https://umedovdilshod.github.io/csb-3es9ym/static/media/Calibre-LightItalic.f34c1430.woff2"
+  },
+  {
+    "revision": "115d37180a9c07e2f480ca81b032babe",
+    "url": "https://umedovdilshod.github.io/csb-3es9ym/static/media/Calibre-Medium.115d3718.woff"
+  },
+  {
+    "revision": "46b36969c7bb1d7ed4c8253e8f274788",
+    "url": "https://umedovdilshod.github.io/csb-3es9ym/static/media/Calibre-Medium.46b36969.woff2"
+  },
+  {
+    "revision": "88178c3f2a309ad11520ee62765072d0",
+    "url": "https://umedovdilshod.github.io/csb-3es9ym/static/media/Calibre-Medium.88178c3f.ttf"
+  },
+  {
+    "revision": "3df14639dd2cb07c83bd39707d70910f",
+    "url": "https://umedovdilshod.github.io/csb-3es9ym/static/media/Calibre-MediumItalic.3df14639.woff2"
+  },
+  {
+    "revision": "4afe8032c647b57de924543c052e0ee4",
+    "url": "https://umedovdilshod.github.io/csb-3es9ym/static/media/Calibre-MediumItalic.4afe8032.woff"
+  },
+  {
+    "revision": "6eb2ac5a1d38f19c7085b131356b355e",
+    "url": "https://umedovdilshod.github.io/csb-3es9ym/static/media/Calibre-MediumItalic.6eb2ac5a.ttf"
+  },
+  {
+    "revision": "96e0a7c47fdd7a8f05007837ead73b35",
+    "url": "https://umedovdilshod.github.io/csb-3es9ym/static/media/Calibre-Regular.96e0a7c4.ttf"
+  },
+  {
+    "revision": "b63c62e591d0c8fbe2c8f009883346d5",
+    "url": "https://umedovdilshod.github.io/csb-3es9ym/static/media/Calibre-Regular.b63c62e5.woff2"
+  },
+  {
+    "revision": "e08232148510aade507c2af9bb66acd0",
+    "url": "https://umedovdilshod.github.io/csb-3es9ym/static/media/Calibre-Regular.e0823214.woff"
+  },
+  {
+    "revision": "35657aa2d12ff78e3d8a8a5ba28c2c35",
+    "url": "https://umedovdilshod.github.io/csb-3es9ym/static/media/Calibre-RegularItalic.35657aa2.woff2"
+  },
+  {
+    "revision": "3a445970c7bcd6b102ac6bd60178b0bc",
+    "url": "https://umedovdilshod.github.io/csb-3es9ym/static/media/Calibre-RegularItalic.3a445970.woff"
+  },
+  {
+    "revision": "c408807a3001d1f7773f5f7fe513c165",
+    "url": "https://umedovdilshod.github.io/csb-3es9ym/static/media/Calibre-RegularItalic.c408807a.ttf"
+  },
+  {
+    "revision": "347808401d7a6607ff9a86897aec0a39",
+    "url": "https://umedovdilshod.github.io/csb-3es9ym/static/media/Calibre-Semibold.34780840.woff"
+  },
+  {
+    "revision": "8325d174436d55e995c7214faafcd47b",
+    "url": "https://umedovdilshod.github.io/csb-3es9ym/static/media/Calibre-Semibold.8325d174.ttf"
+  },
+  {
+    "revision": "d8b856473a51d7eec2a9bccf2fbea54c",
+    "url": "https://umedovdilshod.github.io/csb-3es9ym/static/media/Calibre-Semibold.d8b85647.woff2"
+  },
+  {
+    "revision": "6473be72d5216d0ec21d273af09b9fa3",
+    "url": "https://umedovdilshod.github.io/csb-3es9ym/static/media/Calibre-SemiboldItalic.6473be72.woff"
+  },
+  {
+    "revision": "71941c0c0420e703494d6e2a62bc12d4",
+    "url": "https://umedovdilshod.github.io/csb-3es9ym/static/media/Calibre-SemiboldItalic.71941c0c.ttf"
+  },
+  {
+    "revision": "8f3e4b3eaed73e67bbc4fc81f308a35f",
+    "url": "https://umedovdilshod.github.io/csb-3es9ym/static/media/Calibre-SemiboldItalic.8f3e4b3e.woff2"
+  },
+  {
+    "revision": "02bbb63875ebdbf463e764bb3d8ff190",
+    "url": "https://umedovdilshod.github.io/csb-3es9ym/static/media/SFMono-Medium.02bbb638.ttf"
+  },
+  {
+    "revision": "7df7729fbc66c426b3500d72bc140f5c",
+    "url": "https://umedovdilshod.github.io/csb-3es9ym/static/media/SFMono-Medium.7df7729f.woff"
+  },
+  {
+    "revision": "99497ab65b5b8a7e5c18b7b3edde5c85",
+    "url": "https://umedovdilshod.github.io/csb-3es9ym/static/media/SFMono-Medium.99497ab6.woff2"
+  },
+  {
+    "revision": "92f9fb02060aabf378f0639de5e033ef",
+    "url": "https://umedovdilshod.github.io/csb-3es9ym/static/media/SFMono-MediumItalic.92f9fb02.ttf"
+  },
+  {
+    "revision": "9fbdbe4e2d56f80d6775b61f9b264df8",
+    "url": "https://umedovdilshod.github.io/csb-3es9ym/static/media/SFMono-MediumItalic.9fbdbe4e.woff"
+  },
+  {
+    "revision": "ae7ea80fb472a7d27aa69b3cf00f487c",
+    "url": "https://umedovdilshod.github.io/csb-3es9ym/static/media/SFMono-MediumItalic.ae7ea80f.woff2"
+  },
+  {
+    "revision": "7faa66605357721067fe1b07d9103015",
+    "url": "https://umedovdilshod.github.io/csb-3es9ym/static/media/SFMono-Regular.7faa6660.woff2"
+  },
+  {
+    "revision": "d59c1702c527525e9939ba06166a3750",
+    "url": "https://umedovdilshod.github.io/csb-3es9ym/static/media/SFMono-Regular.d59c1702.woff"
+  },
+  {
+    "revision": "d892da9f8895428033ec68a6c0426ba6",
+    "url": "https://umedovdilshod.github.io/csb-3es9ym/static/media/SFMono-Regular.d892da9f.ttf"
+  },
+  {
+    "revision": "502441ff6ee56a1df1558e8461050a46",
+    "url": "https://umedovdilshod.github.io/csb-3es9ym/static/media/SFMono-RegularItalic.502441ff.woff"
+  },
+  {
+    "revision": "8eddf7adea62d66b9e4d031b58dfaaa6",
+    "url": "https://umedovdilshod.github.io/csb-3es9ym/static/media/SFMono-RegularItalic.8eddf7ad.ttf"
+  },
+  {
+    "revision": "bb1fc3ab1772befac3587b30ca876a09",
+    "url": "https://umedovdilshod.github.io/csb-3es9ym/static/media/SFMono-RegularItalic.bb1fc3ab.woff2"
+  },
+  {
+    "revision": "2d4bff46a7a9ba9dc7701f75726a7815",
+    "url": "https://umedovdilshod.github.io/csb-3es9ym/static/media/SFMono-Semibold.2d4bff46.woff"
+  },
+  {
+    "revision": "8282ed99a84b8e9c803baf3df5b76c01",
+    "url": "https://umedovdilshod.github.io/csb-3es9ym/static/media/SFMono-Semibold.8282ed99.ttf"
+  },
+  {
+    "revision": "cebe2bfbe8a283427e3c5ad8a1b8ae2f",
+    "url": "https://umedovdilshod.github.io/csb-3es9ym/static/media/SFMono-Semibold.cebe2bfb.woff2"
+  },
+  {
+    "revision": "2fd68bed64aeaceadc6402efc4d82767",
+    "url": "https://umedovdilshod.github.io/csb-3es9ym/static/media/SFMono-SemiboldItalic.2fd68bed.woff2"
+  },
+  {
+    "revision": "41da0311eaa7077546785a822a6ba21a",
+    "url": "https://umedovdilshod.github.io/csb-3es9ym/static/media/SFMono-SemiboldItalic.41da0311.woff"
+  },
+  {
+    "revision": "ca70ba8a9030ed9bb7e081f63fd3831b",
+    "url": "https://umedovdilshod.github.io/csb-3es9ym/static/media/SFMono-SemiboldItalic.ca70ba8a.ttf"
+  },
+  {
+    "revision": "4f85a54f4c5c0497d8e0d9e9fdb2669a",
+    "url": "https://umedovdilshod.github.io/csb-3es9ym/static/media/myimg.4f85a54f.jpg"
+  },
+  {
+    "revision": "67498649f84a889bada599aa3b5da312",
+    "url": "https://umedovdilshod.github.io/csb-3es9ym/static/media/project1.67498649.png"
+  },
+  {
+    "revision": "33a2b0bab01d691e4f51512de502b029",
+    "url": "https://umedovdilshod.github.io/csb-3es9ym/static/media/project2.33a2b0ba.PNG"
+  }
+]);
